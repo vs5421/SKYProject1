bfb1ee389c0495f47e57046b52a17f2c302e330e
尹泽方 <AdophKevin@outlook.com>
2017-04-12 22:29:28 +0800

    添加了查询客户信息FrmQuery窗体

---
+++ SKYProject1/frmClientQuery.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace SKYProject1
+{
+    partial class frmClientQuery
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.lblNo = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.txtNo = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.lblSex = new System.Windows.Forms.Label();
+            this.cboSex = new System.Windows.Forms.ComboBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Location = new System.Drawing.Point(27, 207);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(75, 23);
+            this.btnQuery.TabIndex = 4;
+            this.btnQuery.Text = "查找";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // lblNo
+            // 
+            this.lblNo.AutoSize = true;
+            this.lblNo.Location = new System.Drawing.Point(16, 29);
+            this.lblNo.Name = "lblNo";
+            this.lblNo.Size = new System.Drawing.Size(41, 12);
+            this.lblNo.TabIndex = 5;
+            this.lblNo.Text = "编号：";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(16, 79);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(41, 12);
+            this.lblName.TabIndex = 6;
+            this.lblName.Text = "姓名：";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(16, 170);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(65, 12);
+            this.lblPhone.TabIndex = 8;
+            this.lblPhone.Text = "联系电话：";
+            // 
+            // txtNo
+            // 
+            this.txtNo.Location = new System.Drawing.Point(95, 26);
+            this.txtNo.Name = "txtNo";
+            this.txtNo.Size = new System.Drawing.Size(121, 21);
+            this.txtNo.TabIndex = 0;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(95, 76);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(121, 21);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(95, 167);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(121, 21);
+            this.txtPhone.TabIndex = 3;
+            // 
+            // lblSex
+            // 
+            this.lblSex.AutoSize = true;
+            this.lblSex.Location = new System.Drawing.Point(16, 126);
+            this.lblSex.Name = "lblSex";
+            this.lblSex.Size = new System.Drawing.Size(41, 12);
+            this.lblSex.TabIndex = 7;
+            this.lblSex.Text = "性别：";
+            // 
+            // cboSex
+            // 
+            this.cboSex.FormattingEnabled = true;
+            this.cboSex.Items.AddRange(new object[] {
+            "男",
+            "女"});
+            this.cboSex.Location = new System.Drawing.Point(95, 123);
+            this.cboSex.Name = "cboSex";
+            this.cboSex.Size = new System.Drawing.Size(121, 20);
+            this.cboSex.TabIndex = 2;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(132, 207);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "取消";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // frmClientQuery
+            // 
+            this.AcceptButton = this.btnQuery;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 242);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.cboSex);
+            this.Controls.Add(this.lblSex);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtNo);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblNo);
+            this.Controls.Add(this.btnQuery);
+            this.Name = "frmClientQuery";
+            this.Text = "查询客户信息";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnQuery;
+        private System.Windows.Forms.Label lblNo;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.TextBox txtNo;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label lblSex;
+        private System.Windows.Forms.ComboBox cboSex;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
